46ad93d423b574cbe38add0e696df2e58b0a50a7
Marcin Leliwa <marcin.leliwa@gmail.com>
2025-05-14 13:32:31 +0200

    feat: Use ipipan/silver-retriever-base-v1.1

--- docker_build.sh
+++ docker_build.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-EXTRA="cpu"
-#EXTRA="gpu"
-MODEL_NAME="ipipan/silver-retriever-base-v1"
+#EXTRA="cpu"
+EXTRA="gpu"
+MODEL_NAME="ipipan/silver-retriever-base-v1.1"
 DOCKER_REGISTRY="europe-west3-docker.pkg.dev/development-428212/docker-eu"
 IMAGE_NAME="chunker"
-IMAGE_VERSION="$EXTRA-0.0.2"
+IMAGE_VERSION="$EXTRA-0.0.3"
 
 
 docker build \
 --build-arg EXTRA="$EXTRA" \
 --build-arg MODEL_NAME="$MODEL_NAME" \
-# --progress=plain \
 --tag $DOCKER_REGISTRY/$IMAGE_NAME:$IMAGE_VERSION .
+# --progress=plain \
 
 docker push $DOCKER_REGISTRY/$IMAGE_NAME:$IMAGE_VERSION
