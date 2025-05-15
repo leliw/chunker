8914bb5f4719855dc4e59d907cc1fd8418ab9c98
Marcin Leliwa <marcin.leliwa@gmail.com>
2025-05-15 08:56:39 +0200

    chore: Build cpu & gpu at once

--- docker_build.sh
+++ docker_build.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
-#EXTRA="cpu"
-EXTRA="gpu"
 MODEL_NAME="ipipan/silver-retriever-base-v1.1"
 DOCKER_REGISTRY="europe-west3-docker.pkg.dev/development-428212/docker-eu"
 IMAGE_NAME="chunker"
-IMAGE_VERSION="$EXTRA-0.0.3"
+# Base image version, EXTRA will be prepended
+IMAGE_BASE_VERSION="0.0.4"
 
+for EXTRA in "cpu" "gpu"
+do
+    echo "Building for $EXTRA..."
+    IMAGE_VERSION="$EXTRA-$IMAGE_BASE_VERSION"
 
-docker build \
---build-arg EXTRA="$EXTRA" \
---build-arg MODEL_NAME="$MODEL_NAME" \
---tag $DOCKER_REGISTRY/$IMAGE_NAME:$IMAGE_VERSION .
-# --progress=plain \
+    docker build \
+    --build-arg EXTRA="$EXTRA" \
+    --build-arg MODEL_NAME="$MODEL_NAME" \
+    --tag $DOCKER_REGISTRY/$IMAGE_NAME:$IMAGE_VERSION .
+    # --progress=plain \
 
-docker push $DOCKER_REGISTRY/$IMAGE_NAME:$IMAGE_VERSION
+    docker push $DOCKER_REGISTRY/$IMAGE_NAME:$IMAGE_VERSION
+done
