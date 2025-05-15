ad3f34b7d26a8ec32ab91fd303a0b24fb11de8b8
Marcin Leliwa <marcin.leliwa@gmail.com>
2025-05-15 12:07:35 +0200

    chore: Docker push both versions

--- docker_build.sh
+++ docker_build.sh
@@ -3,12 +3,12 @@
 MODEL_NAME="ipipan/silver-retriever-base-v1.1"
 DOCKER_REGISTRY="europe-west3-docker.pkg.dev/development-428212/docker-eu"
 IMAGE_NAME="chunker"
-# Base image version, EXTRA will be prepended
-IMAGE_BASE_VERSION="0.0.4"
+IMAGE_BASE_VERSION="0.0.5"
 
 for EXTRA in "cpu" "gpu"
 do
     echo "Building for $EXTRA..."
+    EXTRA="$EXTRA"
     IMAGE_VERSION="$EXTRA-$IMAGE_BASE_VERSION"
 
     docker build \
@@ -16,6 +16,14 @@ do
     --build-arg MODEL_NAME="$MODEL_NAME" \
     --tag $DOCKER_REGISTRY/$IMAGE_NAME:$IMAGE_VERSION .
     # --progress=plain \
+
+    # docker push $DOCKER_REGISTRY/$IMAGE_NAME:$IMAGE_VERSION
+done
+
+for EXTRA in "cpu" "gpu"
+do
+    echo "Pushing $EXTRA..."
+    IMAGE_VERSION="$EXTRA-$IMAGE_BASE_VERSION"
 
     docker push $DOCKER_REGISTRY/$IMAGE_NAME:$IMAGE_VERSION
 done
