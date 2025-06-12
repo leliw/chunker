48f243b875ccf9a0c5cc2dd361f945dcf322a9a8
Marcin Leliwa <marcin.leliwa@gmail.com>
2025-06-12 09:14:41 +0200

    feat(log_config) Set logging levels by environment variables

--- app/log_config.py
+++ app/log_config.py
@@ -1,11 +1,35 @@
 import logging
-import uvicorn.logging
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class LogConfig(BaseSettings):
+    model_config = SettingsConfigDict(env_prefix="LOG_")
+    level: str = "INFO"
+    log_config: str = "INFO"
+    # Loggers
+
+
+_log = logging.getLogger(__name__)
 
 
 def setup_logging():
-    ch = logging.StreamHandler()
-    ch.setFormatter(
-        uvicorn.logging.DefaultFormatter("%(levelprefix)s %(name)s: %(message)s")
-    )
+    root_logger = logging.getLogger()
+    ch = root_logger.handlers[0] if len(root_logger.handlers) > 0 else logging.StreamHandler()
+    try:
+        # Set FastAPI like formater
+        import uvicorn.logging
+
+        formatter = uvicorn.logging.DefaultFormatter("%(levelprefix)s %(name)s: %(message)s")
+    except ImportError:
+        formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+    ch.setFormatter(formatter)
     logging.getLogger().addHandler(ch)
-    logging.getLogger().setLevel(logging.INFO)
+
+    logconfig = LogConfig()
+    for k, v in logconfig.model_dump().items():
+        name = k.replace("__", ".") if k != "level" else None
+        logging.getLogger(name).setLevel(v)
+        _log.debug("Logging %s -> %s", name, v)
+
+    _log.debug("Logging configured")
