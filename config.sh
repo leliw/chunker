3a4b53547e50d4e61a82ea0e48361c2cf7818219
Marcin Leliwa <marcin.leliwa@gmail.com>
2025-10-21 21:29:50 +0200

    chore: Update libraries

--- config.sh
+++ config.sh
@@ -9,7 +9,9 @@ export DOCKER_REGISTRY="${REGION}-docker.pkg.dev/development-428212/docker-eu"
 export SERVICE_NAME="chunker"
 
 export PROJECT_NUMBER=$(gcloud projects list --filter="project_id=$PROJECT_ID" --format="value(project_number)")
-export SERVICE_ACCOUNT="${PROJECY_NUMBER}-compute@developer.gserviceaccount.com"
+export SERVICE_ACCOUNT="${PROJECT_NUMBER}-compute@developer.gserviceaccount.com"
+
+export SERVICE_ENDPOINT="https://${SERVICE_NAME}-${PROJECT_NUMBER}.${REGION}.run.app"
 
 export IMAGE_NAME="${SERVICE_NAME}"
 export IMAGE_BASE_VERSION=$(uv run app/version.py)
